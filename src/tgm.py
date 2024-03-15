293eddec1a638c8a2569f11e5f861c383ae9eadd
Bernard Costa <bernard.costa@virail.com>
2024-03-15 16:31:52 +0100

    added chat integration to telegram

--- src/tgm.py
+++ src/tgm.py
@@ -4,9 +4,11 @@ from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, Messa
 import os
 import dotenv
 from functools import wraps
+import requests
 
 dotenv.load_dotenv()
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.message.from_user.id
@@ -15,9 +17,25 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat_id = update.message.from_user.id
-    if int(chat_id) == int(os.environ["OWNER_ID"]):
-        await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    try:
+        chat_id = update.message.from_user.id
+        if int(chat_id) == int(os.environ["OWNER_ID"]):
+            prompt = update.message.text
+            url = os.environ["OLLAMA_API_URL"] + "/chat"
+            data = {
+                "model": os.environ["OLLAMA_MODEL"],
+                "messages": [{"role": "user", "content": prompt}],
+                "stream": "false",
+            }
+            logging.info("Requesting Chat with Model...")
+            response = requests.post(url=url, data=data)
+            response.raise_for_status()
+            output = response.json()["message"]["content"]
+            await context.bot.send_message(chat_id=update.effective_chat.id, text=output)
+
+    except requests.exceptions.HTTPError as e:
+        logging.error("Request Failed", str(e))
+
 
 if __name__ == "__main__":
     application = ApplicationBuilder().token(os.environ["TELEGRAM_TOKEN"]).build()
