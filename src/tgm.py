fb3c2a3d2391e2600a1a2f20133c1514d8397e7f
Bernard Costa <bernard.costa@virail.com>
2024-03-15 17:10:35 +0100

    error response handling

--- src/tgm.py
+++ src/tgm.py
@@ -24,20 +24,25 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
         chat_id = update.message.from_user.id
         if int(chat_id) == int(os.environ["OWNER_ID"]):
             prompt = update.message.text
+            logging.info("prompt: " + prompt)
             url = os.environ["OLLAMA_API_URL"] + "/chat"
             data = {
                 "model": os.environ["OLLAMA_MODEL"],
-                "messages": [{"role": "user", "content": prompt}],
-                "stream": "false",
+                "messages": [{"role": "user", "content": str(prompt)}],
+                "stream": False,
+                "keep_alive": "10m"
             }
+            print(data)
             logging.info("Requesting Chat with Model...")
-            response = requests.post(url=url, data=data, timeout=3200)
+            response = requests.post(url=url, json=data, timeout=3200)
             response.raise_for_status()
             output = response.json()["message"]["content"]
             await context.bot.send_message(chat_id=update.effective_chat.id, text=output)
 
     except requests.exceptions.HTTPError as e:
         logging.error("Request Failed", str(e))
+        output = "I'm sorry something went wrong. I canno't help you right now!"
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=output)
 
 
 if __name__ == "__main__":
